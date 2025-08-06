1b73f9bc28a1dbf97ccbe3bff67ce023e8d5edec
cltpalmer <stevennjpalmer@gmail.com>
2025-08-06 16:18:26 -0400

    Update Layout.jsx

--- gShellRelay/src/Layout.jsx
+++ gShellRelay/src/Layout.jsx
@@ -1,7 +1,7 @@
 // src/Layout.jsx
 import SendNav from './components/SendNav';
 import { Outlet } from 'react-router-dom';
-import NavBar from '../../../shared/components/navBar';
+import NavBar from './components/navBar'; // Local component
 export default function Layout() {
   return (
     <>
