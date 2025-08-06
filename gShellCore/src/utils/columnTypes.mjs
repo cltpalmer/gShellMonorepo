83f2b999cfc4227ca64870027863c176be5de789
cltpalmer <stevennjpalmer@gmail.com>
2025-08-06 16:31:43 -0400

    Add files via upload

---
+++ gShellCore/src/utils/columnTypes.mjs
@@ -0,0 +1,24 @@
+// shared/columnTypes.mjs
+export const allowedTypes = [
+  "string", 
+  "number", 
+  "bool", 
+  "timestamp", 
+  "text", 
+  "email", 
+  "date",
+  "image",
+  "list"
+];
+
+export const typeLabels = {
+  string: "Short Text",
+  number: "Number",
+  bool: "Checkbox",
+  timestamp: "Date+Time",
+  text: "Long Text",
+  email: "Email",
+  date: "Date",
+  image: "image",
+  list: "List"
+};
